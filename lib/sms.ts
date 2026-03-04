b684ba08cf1779a8ab88d429fab9667d31bbfc0a
holtzshedden <alex@holtzshedden.com>
2026-03-04 17:27:32 +0100

    Delete lib directory

--- lib/sms.ts
+++
@@ -1,9 +0,0 @@
-export async function sendPickupSms(params: {
-  toPhone: string;
-  link: string;
-  storeName: string;
-}) {
-  // MVP: nur stub, später Twilio o.ä.
-  console.log("[PickPass SMS stub]", params);
-  return { ok: true };
-}
--- lib/token.ts
+++
@@ -1,8 +0,0 @@
-export function generateToken(length = 8) {
-  const alphabet = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
-  let out = "";
-  for (let i = 0; i < length; i++) {
-    out += alphabet[Math.floor(Math.random() * alphabet.length)];
-  }
-  return out;
-}
