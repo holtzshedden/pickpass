2db4762a3b0ff9a0ddce93ca985dbb264a59c10e
holtzshedden <alex@holtzshedden.com>
2026-03-04 18:39:42 +0100

    Create auth.ts

---
+++ app/lib/auth.ts
@@ -0,0 +1,29 @@
+// app/lib/auth.ts
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+export async function requireUserId() {
+  const { userId } = auth();
+  if (!userId) {
+    const err = new Error("Unauthorized");
+    // @ts-ignore
+    err.status = 401;
+    throw err;
+  }
+  return userId;
+}
+
+export async function requireOwner() {
+  const userId = await requireUserId();
+
+  const user = await clerkClient.users.getUser(userId);
+  const flag = (user.publicMetadata as any)?.pickpassOwner;
+
+  if (flag !== true) {
+    const err = new Error("Forbidden");
+    // @ts-ignore
+    err.status = 403;
+    throw err;
+  }
+
+  return { userId, user };
+}
